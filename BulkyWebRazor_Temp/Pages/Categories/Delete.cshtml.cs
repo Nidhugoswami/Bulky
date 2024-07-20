3c863f93b1ab1d7e87e9141c998605af92ec9eec
Nidhi Goswami <nidhi29goswami@gmail.com>
2024-07-20 19:09:16 +0530

    Part-2 Category CRUD Razor Pages

---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]  //process that takes values from HTTP requests and maps them to handler method parameters or PageModel properties. Model binding reduces the need for the developer to manually extract values from the request and then assign them, one by one, to variables or properties for later processing. 
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicatiobDbContext _db;
+        public Category Category { get; set; }
+        public DeleteModel(ApplicatiobDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null || id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? obj = _db.Categories.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToPage("Index");
+        }
+        
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicatiobDbContext _db;
+        public Category Category { get; set; }
+        public EditModel(ApplicatiobDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int ? id)
+        {
+            if(id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid) 
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
